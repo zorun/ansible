e739bb09711ee674bb3200de0126bc940591f5b5
Baptiste Jonglez <git@bitsofnetworks.org>
2021-08-07 01:05:22 +0200

    Add uptime facts on NetBSD

--- lib/ansible/module_utils/facts.py
+++ lib/ansible/module_utils/facts.py
@@ -2034,6 +2034,7 @@ class NetBSDHardware(Hardware):
     - processor_cores
     - processor_count
     - devices
+    - uptime_seconds
     """
     platform = 'NetBSD'
     MEMORY_FACTS = ['MemTotal', 'SwapTotal', 'MemFree', 'SwapFree']
@@ -2042,6 +2043,7 @@ class NetBSDHardware(Hardware):
         self.sysctl = self.get_sysctl(['machdep'])
         self.get_cpu_facts()
         self.get_memory_facts()
+        self.get_uptime_facts()
         try:
             self.get_mount_facts()
         except TimeoutError:
@@ -2089,6 +2091,22 @@ class NetBSDHardware(Hardware):
             if key in NetBSDHardware.MEMORY_FACTS:
                 val = data[1].strip().split(' ')[0]
                 self.facts["%s_mb" % key.lower()] = int(val) // 1024
+
+    def get_uptime_facts(self):
+        # On NetBSD, we need to call it with -n to get this value as an int.
+        sysctl_cmd = self.module.get_bin_path('sysctl')
+        cmd = [sysctl_cmd, '-n', 'kern.boottime']
+
+        rc, out, err = self.module.run_command(cmd)
+
+        if rc != 0:
+            return
+
+        kern_boottime = out.strip()
+        if not kern_boottime.isdigit():
+            return
+
+        self.facts['uptime_seconds'] = int(time.time() - int(kern_boottime))
 
     @timeout()
     def get_mount_facts(self):
