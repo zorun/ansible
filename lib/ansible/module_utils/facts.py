8059cda0e518407e95e97c1767876da04833594b
Baptiste Jonglez <git@bitsofnetworks.org>
2021-08-07 00:43:34 +0200

    facts: Add uptime fact for FreeBSD and Darwin
    This is a bit more complicated than on OpenBSD/NetBSD.
    
    The code is inspired from https://github.com/Cairnarvon/uptime
    
    Tested on the GCC compile farm: https://cfarm.tetaneutral.net/

--- lib/ansible/module_utils/facts.py
+++ lib/ansible/module_utils/facts.py
@@ -1885,6 +1885,7 @@ class FreeBSDHardware(Hardware):
     - processor_cores
     - processor_count
     - devices
+    - uptime_seconds
     """
     platform = 'FreeBSD'
     DMESG_BOOT = '/var/run/dmesg.boot'
@@ -1894,6 +1895,7 @@ class FreeBSDHardware(Hardware):
         self.get_memory_facts()
         self.get_dmi_facts()
         self.get_device_facts()
+        self.get_uptime_facts()
         try:
             self.get_mount_facts()
         except TimeoutError:
@@ -1967,6 +1969,22 @@ class FreeBSDHardware(Hardware):
                 s = slices.match(device)
                 if s:
                     self.facts['devices'][d.group(1)].append(s.group(1))
+
+    def get_uptime_facts(self):
+        # On FreeBSD, the default format is annoying to parse.
+        # Use -b to get the raw value and decode it.
+        sysctl_cmd = self.module.get_bin_path('sysctl')
+        cmd = [sysctl_cmd, '-b', 'kern.boottime']
+
+        rc, out, err = self.module.run_command(cmd)
+
+        if rc != 0 or len(out) < struct.calcsize('@LL'):
+            return
+
+        # Returns seconds and microseconds as two different fields
+        (kern_boottime, _)  = struct.unpack('@LL', out)
+
+        self.facts['uptime_seconds'] = int(time.time()) - kern_boottime
 
     def get_dmi_facts(self):
         ''' learn dmi facts from system
@@ -2398,6 +2416,7 @@ class Darwin(Hardware):
     - model
     - osversion
     - osrevision
+    - uptime_seconds
     """
     platform = 'Darwin'
 
@@ -2406,6 +2425,7 @@ class Darwin(Hardware):
         self.get_mac_facts()
         self.get_cpu_facts()
         self.get_memory_facts()
+        self.get_uptime_facts()
         return self.facts
 
     def get_system_profile(self):
@@ -2441,6 +2461,22 @@ class Darwin(Hardware):
         rc, out, err = self.module.run_command("sysctl hw.usermem")
         if rc == 0:
             self.facts['memfree_mb'] = int(out.splitlines()[-1].split()[1]) // 1024 // 1024
+
+    def get_uptime_facts(self):
+        # This works exactly like on FreeBSD.
+        # Use -b to get the raw value and decode it.
+        sysctl_cmd = self.module.get_bin_path('sysctl')
+        cmd = [sysctl_cmd, '-b', 'kern.boottime']
+
+        rc, out, err = self.module.run_command(cmd)
+
+        if rc != 0 or len(out) < struct.calcsize('@LL'):
+            return
+
+        # Returns seconds and microseconds as two different fields
+        (kern_boottime, _)  = struct.unpack('@LL', out)
+
+        self.facts['uptime_seconds'] = int(time.time()) - kern_boottime
 
 class HurdHardware(LinuxHardware):
     """
